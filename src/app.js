c443b3feae037f910ebb72ff903c6f825a52d161
Anniel Menezes <annielmenezes@ideiaseo.com>
2017-12-11 17:30:58 -0300

    feat (app audits) Add logger to application

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
+import { createWriteStream } from "fs";
+import path from "path";
 import express from "express";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 import routes from "./routes";
 import { connect } from "../config/database";
 
 const app = express();
+const accessLogStream = createWriteStream(
+  path.join(__dirname, "../logs/access.log"),
+  { flags: "a" }
+);
 const register = (...middlewares) => app =>
   middlewares.forEach(middleware => app.use(middleware));
-
 const configureExpress = () => {
-  register(bodyParser.json(), routes)(app);
+  register(
+    bodyParser.json(),
+    bodyParser.urlencoded({ extended: true }),
+    morgan("combined", { stream: accessLogStream }),
+    routes
+  )(app);
   return app;
 };
 
